c9adc0a00dce196649492e67776e6ed4c3707008
mtlvsdn <matei.popescu@outlook.de>
2024-12-01 17:24:15 +0200

    I added the functionality to add notes in the app and did more css styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
 import './App.css';
+import { useState } from 'react'
+
+// const Note = {
+//   id: Number;
+//   title: String;
+//   content: String
+// };
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "Title 1",
+      content: "Content 1",
+    },
+    {
+      id: 2,
+       title: "Title 2",
+      content: "Content 2",
+    },
+    {
+      id: 3,
+      title: "Title 3",
+      content: "Content 3",
+    },
+    {
+      id: 4,
+      title: "Title 4",
+      content: "Content 4",
+    } 
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("title: ", title);
+    console.log("content: ", content);
+
+    const newNote = {
+      id: notes.length+1,
+      title: title,
+      content: content,
+    }
+
+    setNotes([newNote, ...notes]);
+    setTitle("");
+    setContent("");
+  };
+
   return (
-   <div className="app-container">
-    <form className="note-form">
+   <div className="app-container"> 
+    <form className="note-form" onSubmit={(event) => handleSubmit(event)}>
       <input
+      value={title}
+      onChange={(event)=>setTitle(event.target.value)}
       placeholder="Title"
       required
       ></input>
       <textarea
+      value={content}
+      onChange={(event)=>setContent(event.target.value)}
       placeholder="Content"
       rows={10}
       required
@@ -16,13 +69,15 @@ function App() {
       <button type="submit">Add Note</button>
     </form>
     <div className="notes-grid">
-      <div className="note-item">
-        <div className="notes-header">
-          <button>x</button>
+      {notes.map((note)=> (
+        <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
         </div>
-        <h2>Note Title</h2>
-        <p>Note Content</p>
-      </div>
+      ))}
     </div>
    </div>
   );
