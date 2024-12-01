5119a664c9f2ea175b2bbea405b841722fdb260e
mtlvsdn <matei.popescu@outlook.de>
2024-12-01 16:34:31 +0200

    I initialised the react project, created the form to write the note and the grid to store all the notes. I started the styling of the elements in CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="app-container">
+    <form className="note-form">
+      <input
+      placeholder="Title"
+      required
+      ></input>
+      <textarea
+      placeholder="Content"
+      rows={10}
+      required
+      ></textarea>
+      <button type="submit">Add Note</button>
+    </form>
+    <div className="notes-grid">
+      <div className="note-item">
+        <div className="notes-header">
+          <button>x</button>
+        </div>
+        <h2>Note Title</h2>
+        <p>Note Content</p>
+      </div>
     </div>
+   </div>
   );
 }
 
